52d9918b69205778fbf8c8c588d26e3e6051e06a
Jonathan Cagua <jonathan.cagua@gmail.com>
2022-07-02 22:34:46 -0500

    ADD: test asm

---
+++ 4_test_point/Debug/Src/subdir.mk
@@ -0,0 +1,51 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+S_SRCS += \
+../Src/SysTick_Handler.s 
+
+C_SRCS += \
+../Src/led.c \
+../Src/main.c \
+../Src/syscalls.c \
+../Src/sysmem.c \
+../Src/timebase.c \
+../Src/uart.c 
+
+OBJS += \
+./Src/SysTick_Handler.o \
+./Src/led.o \
+./Src/main.o \
+./Src/syscalls.o \
+./Src/sysmem.o \
+./Src/timebase.o \
+./Src/uart.o 
+
+S_DEPS += \
+./Src/SysTick_Handler.d 
+
+C_DEPS += \
+./Src/led.d \
+./Src/main.d \
+./Src/syscalls.d \
+./Src/sysmem.d \
+./Src/timebase.d \
+./Src/uart.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Src/%.o: ../Src/%.s Src/subdir.mk
+	arm-none-eabi-gcc -mcpu=cortex-m4 -g3 -DDEBUG -c -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@" "$<"
+Src/%.o Src/%.su: ../Src/%.c Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DSTM32L475xx -c -I../Inc -I"/Users/jonathancagua/STM32/test-stm32-os/4_test_point/chip_headers/CMSIS/Include" -I"/Users/jonathancagua/STM32/test-stm32-os/4_test_point/chip_headers/CMSIS/Device/ST/STM32L4xx/Include" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Src
+
+clean-Src:
+	-$(RM) ./Src/SysTick_Handler.d ./Src/SysTick_Handler.o ./Src/led.d ./Src/led.o ./Src/led.su ./Src/main.d ./Src/main.o ./Src/main.su ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su ./Src/timebase.d ./Src/timebase.o ./Src/timebase.su ./Src/uart.d ./Src/uart.o ./Src/uart.su
+
+.PHONY: clean-Src
+
---
+++ 4_test_point/Src/main.c
@@ -0,0 +1,32 @@
+/**
+ * @file main.c
+ * @author Jonathan Cagua(jonathan.cagua@gmail.com)
+ * @brief testing de componentes
+ * @version 0.1
+ * @date 2022-07-02
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <stdint.h>
+#include "led.h"
+#include "uart.h"
+#include "timebase.h"
+
+uint32_t sp_antes, sp_durante, sp_despues;
+uint32_t stackFrame[8];
+
+int main(void)
+{
+	led_init();
+	uart_tx_init();
+	tick_init();
+    /* Loop forever */
+	while (1) {
+		asm ("mrs %[sp_antes], MSP" : [sp_antes] "=r" (sp_antes));
+		__WFI();
+		asm ("mrs %[sp_despues], MSP" : [sp_despues] "=r" (sp_despues));
+	}
+}
+
