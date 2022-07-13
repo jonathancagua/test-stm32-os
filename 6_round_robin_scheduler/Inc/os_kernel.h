4d1d2ae92822f2fabea748d53b146b0c6583bf3b
Jonathan Cagua <jonathan.cagua@gmail.com>
2022-07-12 22:01:35 -0500

    ADD: header os

--- 6_round_robin_scheduler/Inc/os_kernel.h
+++ 6_round_robin_scheduler/Inc/os_kernel.h
@@ -10,4 +10,7 @@
 #include <stdint.h>
 #include "stm32l4xx.h"
 
+void os_kernel_init(void);
+void os_kernel_launch(uint32_t quanta);
+uint8_t os_kernel_add_threads(void(*task_0)(void),void(*task_1)(void),void(*task_2)(void));
 #endif /* OS_KERNEL_H_ */
