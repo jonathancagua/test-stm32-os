52174770182d9aa004a8f94d95e0e22ce00ac082
Jonathan Cagua <jonathan.cagua@gmail.com>
2022-08-07 20:29:53 -0500

    ADD: ejemplo de clase 4

--- 8_signal/Core/Src/class4_1.c
+++ 8_signal/Core/Src/class4_1.c
@@ -11,7 +11,8 @@
 #include "dac.h"
 #include "usart.h"
 #define NUCLEO_CLOCK_SPEED 80000000U
-#define BITS 4 //10
+#define BITS    10   // cantidad de bits usado para cuantizar
+#define CUTFREC 500 // frec de corte para filtrar en frec
 uint32_t tick   = 0   ;
 uint16_t tone   = 440 ;
 uint16_t B      = 4000;
@@ -45,66 +46,132 @@ static void trigger(int16_t threshold)
       ;
    return;
 }
-//void init_cfft_instance(arm_cfft_instance_q15* CS,int length)/*{{{*/
-//{
-//   switch(length){
-//      case 16:
-//         *CS=arm_cfft_sR_q15_len16;
-//         break;
-//      case 32:
-//         *CS=arm_cfft_sR_q15_len32;
-//         break;
-//      case 64:
-//         *CS=arm_cfft_sR_q15_len64;
-//         break;
-//      case 128:
-//         *CS=arm_cfft_sR_q15_len128;
-//         break;
-//      case 256:
-//         *CS=arm_cfft_sR_q15_len256;
-//         break;
-//      case 512:
-//         *CS=arm_cfft_sR_q15_len512;
-//         break;
-//      case 1024:
-//         *CS=arm_cfft_sR_q15_len1024;
-//         break;
-//      case 2048:
-//         *CS=arm_cfft_sR_q15_len2048;
-//         break;
-//      case 4096:
-//         *CS=arm_cfft_sR_q15_len4096;
-//         break;
-//      default:
-//         *CS=arm_cfft_sR_q15_len128;
-//   }
-//   return;
-//}
+void init_cfft_instance(arm_cfft_instance_q15* CS,int length)/*{{{*/
+{
+   switch(length){
+      case 16:
+         *CS=arm_cfft_sR_q15_len16;
+         break;
+      case 32:
+         *CS=arm_cfft_sR_q15_len32;
+         break;
+      case 64:
+         *CS=arm_cfft_sR_q15_len64;
+         break;
+      case 128:
+         *CS=arm_cfft_sR_q15_len128;
+         break;
+      case 256:
+         *CS=arm_cfft_sR_q15_len256;
+         break;
+      case 512:
+         *CS=arm_cfft_sR_q15_len512;
+         break;
+      case 1024:
+         *CS=arm_cfft_sR_q15_len1024;
+         break;
+      case 2048:
+         *CS=arm_cfft_sR_q15_len2048;
+         break;
+      case 4096:
+         *CS=arm_cfft_sR_q15_len4096;
+         break;
+      default:
+         *CS=arm_cfft_sR_q15_len128;
+   }
+   return;
+}
+
+void clearFloatBuf(float *b,uint16_t len)
+{
+   while(len--)
+      b[len]=0.0;
+}
 
 void class1_main(){
 	uint16_t sample = 0;
 	int16_t adc [ header.N ];
+	arm_cfft_instance_q15 CS;
+	q15_t fftInOut[ header.N *2 ];//
 	float t;
 	cyclesCounterInit();
 	while(1) {
 		cyclesCounterReset();
-		t =((tick)/(float)header.fs);
-		tick++;
-		q15_t q_sample = 512*arm_sin_f32(t*tone*2*PI);
-		#ifdef original
-		adc[sample] = q_sample << 6;
-		#else
-		adc[sample] = (((adcRead()-512))>>(10-BITS))<<(6+(10-BITS));
-		#endif
 		HAL_UART_Transmit(&huart1, (uint8_t* )&adc[sample], sizeof(adc[0]), 1);
-		dacWrite(q_sample+512); // sweept
+		HAL_UART_Transmit(&huart1, (uint8_t* )&fftInOut[sample*2],sizeof(fftInOut[0]), 1);
+		adc[sample]       = (((int16_t )adcRead()-512)>>(10-BITS))<<(6+10-BITS);
+		fftInOut[sample*2]   = adc[sample];
+		fftInOut[sample*2+1] = 0;
+
 		if ( ++sample==header.N ) {
-			arm_max_q15 ( adc, header.N, &header.maxValue,&header.maxIndex );
-			arm_min_q15 ( adc, header.N, &header.minValue,&header.minIndex );
-			arm_rms_q15 ( adc, header.N, &header.rms                       );
-			header.id++;
-			HAL_UART_Transmit(&huart1, (uint8_t* )&header, sizeof(header), 1);
 			sample = 0;
+			//------------TRANSFORMADA------------------
+			init_cfft_instance ( &CS,header.N        );
+			arm_cfft_q15       ( &CS ,fftInOut ,0 ,1 ); //0 directa, 1 inversa
+
+			// FILTRADO BASICO RECORTANDO EN FREC
+			fftInOut[0]=0; //elimino la continua
+			fftInOut[1]=0;
+			int cutBin=CUTFREC/(header.fs/header.N); //defino el n donde comenzar a cortar
+			for(int i=0;i<=(header.N/2);i++) {        //solo recorro la mitad, porque la otra mitad es compleja conjugada asi que borro de los 2 lados
+			  if(i>=cutBin ) {
+				 fftInOut[i*2]                  = 0; //borro bin parte real
+				 fftInOut[i*2+1]                = 0; //borro bin parte compleja
+				 fftInOut[(header.N-1)*2-i*2]   = 0; //lo mismo pero de atras para adelante
+				 fftInOut[(header.N-1)*2-i*2+1] = 0;
+			  }
+			}
+			//---------ANTI transformada---------------------
+			init_cfft_instance ( &CS,header.N        );
+			arm_cfft_q15       ( &CS ,fftInOut ,1 ,1 );
+
+			header.id++;
+			HAL_UART_Transmit(&huart1, (uint8_t* )&header, sizeof(struct header_struct ), 1);
+
+		}
+		while(cyclesCounterRead() < NUCLEO_CLOCK_SPEED/header.fs) // el clk de la nucleo es 80M
+		;
+	}
+
+}
+
+void class2_main(){
+	uint16_t sample = 0;
+	int16_t adc [ header.N ];
+	arm_cfft_instance_q15 CS;
+	q15_t fftInOut[ header.N *2 ];//
+	float coorIn [ header.N      ];// guarda copia de samples en Q15 como in para la fft.La fft corrompe los datos de la entrada!
+	float corrOut[ header.N *2   ];
+	float t;
+	int i;
+	float signal [ header.N      ];
+	cyclesCounterInit();
+	clearFloatBuf(signal,header.N);
+	for(i=0;i<10;i++) {
+	  signal[i]=-i/10.0;
+	}
+	for(;i<20;i++) {
+	  signal[i]=(i-10)/10.0;
+	}
+	while(1) {
+		cyclesCounterReset();
+		int16_t adcRaw;
+		HAL_UART_Transmit(&huart1, (uint8_t* )&adc[sample], sizeof(adc[0]), 1);// envia el sample ANTERIOR
+		HAL_UART_Transmit(&huart1, (uint8_t* )&corrOut[sample*2], sizeof(corrOut[0]), 1);
+		HAL_UART_Transmit(&huart1, (uint8_t* )&corrOut[sample*2+1], sizeof(corrOut[0]), 1);
+		adcRaw        = adcRead()-512;
+		adc[sample]   = adcRaw/512.0;             // PISA el sample que se acaba de mandar con una nueva muestra
+		coorIn[sample] = adcRaw;
+
+
+		if ( ++sample==header.N ) {
+			clearFloatBuf ( corrOut,2*header.N );
+			arm_correlate_f32 ( signal,header.N,adc ,header.N,corrOut  );
+			// trigger(2);
+			sample = 0;
+			header.id++;
+			HAL_UART_Transmit(&huart1, (uint8_t* )&header, sizeof(struct header_struct ), 1);
+
 		}
 		while(cyclesCounterRead() < NUCLEO_CLOCK_SPEED/header.fs) // el clk de la nucleo es 80M
 		;
--- 8_signal/Core/Src/main.c
+++ 8_signal/Core/Src/main.c
@@ -111,7 +111,7 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-	//app3_main();
+	class1_main();
 	HAL_Delay(1000);
   }
   /* USER CODE END 3 */
