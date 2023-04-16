e5d2c8bcd42e51a334a1658f101c784755f2c735
Jonathan Cagua <jonathan.cagua@gmail.com>
2023-04-16 10:55:55 -0500

    ADD: ejemplo para visualizar osciloscopio

--- test_rtos_pid/Core/Src/pid.c
+++ test_rtos_pid/Core/Src/pid.c
@@ -4,12 +4,13 @@
  *  Created on: Apr 9, 2023
  *      Author: jonathancagua
  */
-
+#include <stdbool.h>
 #include "arm_math.h"
 #include "pid.h"
 #include "app_adc.h"
 #include "FreeRTOS.h"
 #include "task.h"
+#include "timers.h"
 /* Choose PID parameters */
 #define PID_PARAM_KP        0.613            /* Proporcional */
 #define PID_PARAM_KI        0.119        	/* Integral */
@@ -26,6 +27,7 @@ arm_pid_instance_f32 PID;
 static uint16_t dacValue = 0;
 static uint32_t dac_convert = 0;
 static uint32_t sample_2 = 0;
+static bool enable_pid = true;
 
 float32_t read_sensor(){
 	float32_t value = 0;
@@ -62,27 +64,42 @@ int app_pid(void)
     {
         // Leer el valor del sensor de la variable "input"
         float32_t input = read_sensor();
+        if(true == enable_pid){
+			// Calcular el error de seguimiento
+			error = set_point - input;
 
-        // Calcular el error de seguimiento
-        error = set_point - input;
+			// Actualizar la salida del controlador PID
+			output = arm_pid_f32(&PID, error);
 
-        // Actualizar la salida del controlador PID
-        output = arm_pid_f32(&PID, error);
-
-		if (output > lim_max) {
-			output = lim_max;
-		} else if (output < lim_min) {
-			output = lim_min;
-		}
-
+			if (output > lim_max) {
+				output = lim_max;
+			} else if (output < lim_min) {
+				output = lim_min;
+			}
+        }
         // Escribir la salida del controlador en el actuador
         write_actuator(output);
+
         printf ("%d / %d \r\n", ((uint16_t)sample_2), ((uint16_t)dac_convert));
         vTaskDelayUntil(&lastWakeTime, pdMS_TO_TICKS(1000 / 1000));
     }
 }
 
+void vTimerCallback(TimerHandle_t xTimer) {
+	if(true == enable_pid){
+		output =0;
+		enable_pid = false;
+	}
+	else{
+		enable_pid = true;
+	}
+
+}
 void init_pid(void) {
     xTaskCreate(app_pid, "task_identification",configMINIMAL_STACK_SIZE*4, NULL, tskIDLE_PRIORITY + 1, NULL);
+    // Crea un temporizador con un tiempo de espera de 5 segundos y una repetición automática
+    TimerHandle_t xTimer = xTimerCreate("Timer Example", pdMS_TO_TICKS(1000), pdTRUE, NULL, vTimerCallback);
 
+    // Inicia el temporizador
+    xTimerStart(xTimer, 0);
 }
